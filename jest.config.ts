737e1bb849ae26a2c8c0403dd80d7928861be028
Naveen S Belandur <146941787+NaveenBelandurHelloMentor@users.noreply.github.com>
2025-05-22 19:21:01 +0530

    StoryBook React Jest Typescritp
    in this repo i have made a small componet of breaking news where i have used storybook for buttons and react jest for testing unit testing

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    images: {
+    domains: ['hellomentor.in', 's3-example-hello-mentor.s3.ap-south-1.amazonaws.com'],
+  },
+  /* config options here */
+};
+
+export default nextConfig;
